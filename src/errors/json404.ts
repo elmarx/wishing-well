46d85474f22d9c75341e9c299956ca6dc54a866c
Elmar Athmer <elmar@athmer.org>
2023-04-01 13:53:40 +0200

    add JSON 404 handler

---
+++ src/errors/json404.ts
@@ -0,0 +1,16 @@
+import { Handler } from "express";
+
+/**
+ * return 404 error handler
+ *
+ * @param disableHtml404Handler disable the default HTML 404 handler from express
+ */
+export function initJson404Handler(disableHtml404Handler = true): Handler {
+  return (req, res, next) => {
+    if (disableHtml404Handler || req.header("application/json")) {
+      return res.status(404).json({ error: "not found" });
+    } else {
+      return next();
+    }
+  };
+}
