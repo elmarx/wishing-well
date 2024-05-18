40e55e4edbde05d19d6e9ab17fe5152834b5d7f3
Elmar Athmer <elmar@athmer.org>
2024-05-18 15:55:23 +0200

    update node 18 -> 20

--- src/handler/loggingMiddleware.ts
+++ src/handler/loggingMiddleware.ts
@@ -2,7 +2,6 @@ import { Handler } from "express";
 import { context, httpLogger } from "../logging";
 
 const contextMiddleware: Handler = (req, _res, next) => {
-  // @ts-expect-error typings for run seem wrong, they require third argument, but it is optional (and not needed here)
   return context.run({ logger: req.log }, next);
 };
 
